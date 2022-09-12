113b6d2c8504158fd42d6e3c4d41e8c66e6214bb
ichbinsieger <oluwaseun.v@yahoo.com>
2022-09-12 23:04:40 +0100

    item page

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import ItemView from '../views/ItemView.vue'
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/item',
+    name: 'item',
+    component: ItemView
   },
   {
     path: '/about',
